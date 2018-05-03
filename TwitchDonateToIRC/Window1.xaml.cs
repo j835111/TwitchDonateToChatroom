0426f234108c268665423f7ae62b003adf8285f7
JamesLin <j835111@gmail.com>
2018-05-03 23:51:19 +0800

    小調整

--- TwitchDonateToIRC/Window1.xaml.cs
+++ TwitchDonateToIRC/Window1.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TwitchDonateToIRC
 {
     /// <summary>
-    /// Page1.xaml 的互動邏輯
+    /// Window1.xaml 的互動邏輯
     /// </summary>
-    public partial class Page1 : Page
+    public partial class Window1 : Window
     {
-        public Page1()
+        public Window1()
         {
             InitializeComponent();
         }
